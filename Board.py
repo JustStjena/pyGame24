bcf586ef4f08a9ed9d22541a98bbbc75a3f1aa14
GAI <GAI@mail.ru>
2023-12-01 18:48:26 +0300

    added mouse click

--- Board.py
+++ Board.py
@@ -27,9 +27,26 @@ class Board:
         '''
         for y in range(self.height):
             for x in range(self.width):
-                pygame.draw.rect(screen, pygame.Color(255, 255, 255), (
-                    x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size,
-                    self.cell_size), 1)
+                if self.board[y][x] == 0:
+                    pygame.draw.rect(screen, pygame.Color(255, 255, 255), (
+                        x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size,
+                        self.cell_size), 1)
+                else:
+                    pygame.draw.rect(screen, pygame.Color(255, 255, 255), (
+                        x * self.cell_size + self.left, y * self.cell_size + self.top, self.cell_size,
+                        self.cell_size))
+
+    def pos2coord(self, pos):
+        x = (pos[0] - self.top) // self.cell_size
+        y = (pos[1] - self.left) // self.cell_size
+        if x > self.width - 1 or y > self.height - 1 or x < 0 or y < 0:
+            return None
+        return x, y
+
+    def on_click(self, pos):
+        coord = self.pos2coord(pos)
+        if coord is not None:
+            self.board[coord[1]][coord[0]] = 1
 
     def set_view(self, left, top, cell_size):
         '''
@@ -50,15 +67,17 @@ if __name__ == '__main__':
     screen = pygame.display.set_mode(SIZE)
     pygame.display.set_caption('Не забываем писать заголовок окна')
     # Инициализация клетчатого поля
-    BOARD_WIDTH, BOARD_HEIGHT = 5, 7
+    BOARD_WIDTH, BOARD_HEIGHT = 3, 4
     board = Board(BOARD_WIDTH, BOARD_HEIGHT)
     board_size = min(WIDTH // BOARD_WIDTH, HEIGHT // BOARD_HEIGHT)
-    board.set_view(0, 0, board_size)
+    board.set_view(100, 100, board_size)
     running = True
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                board.on_click(event.pos)
         screen.fill((0, 0, 0))
         board.render(screen)
         pygame.display.flip()
